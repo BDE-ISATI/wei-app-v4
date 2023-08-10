d6a98fa73d0ea56f08115a5898e3bc8647a6e55a
Théo Le Goc <theo@legoc.net>
2023-08-10 12:35:19 +0200

    Added cognito url and user pool id in AppConfig.tsx

--- src/Config/AppConfig.tsx
+++ src/Config/AppConfig.tsx
@@ -1,4 +1,6 @@
 const URL = "https://xrprowiv1i.execute-api.eu-west-1.amazonaws.com";
+const cognitoURL = "https://cognito-idp.eu-west-1.amazonaws.com/";
+const cognitoUserPoolClientId = "600c0salfhpv788iis2g61d69n";
 
 const apiUrls = {
   dev: URL + "/dev/",
