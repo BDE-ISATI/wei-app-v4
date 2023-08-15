f4437e3709af9e421134e7160e6f6bbdb9d26a49
Théo Le Goc <theo@legoc.net>
2023-08-15 16:34:54 +0200

    Added challenge requests, validation, creation, update and get + models

--- src/Transforms/index.tsx
+++ src/Transforms/index.tsx
@@ -1,2 +1,4 @@
 export type { IRequestError } from "./ApiError";
 export type { IUserData } from "./User";
+export type { IResult } from "./Result";
+export type { IChallengeData, IChallengeUpdateData } from "./Challenge"
