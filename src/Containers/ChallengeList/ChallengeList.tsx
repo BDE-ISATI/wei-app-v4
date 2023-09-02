1802194b5d4a5cc4f21a3fe9bf62d6ba619846df
Théo Le Goc <theo@legoc.net>
2023-09-02 12:27:16 +0200

    Beginning advanced filters

--- src/Containers/ChallengeList/ChallengeList.tsx
+++ src/Containers/ChallengeList/ChallengeList.tsx
@@ -12,13 +12,17 @@ import {
   TextField,
   useTheme,
   ToggleButtonGroup,
-  ToggleButton
+  ToggleButton, Select,
+  MenuItem, Accordion, AccordionSummary, AccordionDetails, Typography,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
 import {IState} from "../../Reducers";
 import CardContent from "@mui/material/CardContent";
+import {faArrowUpShortWide, faArrowDownWideShort, faFilter} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {ArrowDownward} from "@mui/icons-material";
 
 const generateChallengeList = (challenges: IChallengeData[] | undefined, searchValue: string, dateFilter: string[]) => {
   if (challenges === undefined) {
@@ -35,16 +39,16 @@ const generateChallengeList = (challenges: IChallengeData[] | undefined, searchV
     if (dateFilter.length === 0)
       return true;
 
-    if (dateFilter.includes("active") && value.end > Date.now()/1000 && value.start < Date.now()/1000)
+    if (dateFilter.includes("active") && value.end > Date.now() / 1000 && value.start < Date.now() / 1000)
       return true;
-    if (dateFilter.includes("upcoming") && value.start > Date.now()/1000)
+    if (dateFilter.includes("upcoming") && value.start > Date.now() / 1000)
       return true;
-    if (dateFilter.includes("finished") && value.end < Date.now()/1000)
+    if (dateFilter.includes("finished") && value.end < Date.now() / 1000)
       return true;
 
     return false;
   }).sort((a, b) => {
-    return a.end - b.end;
+    return (dateFilter.includes("upcoming") && dateFilter.length === 1) ? a.start - b.start : a.end - b.end;
   }).map((data, index) => (
     <div key={index}>
       <ChallengeCard challengeData={data}/>
@@ -58,6 +62,7 @@ const ChallengeList = () => {
   >();
   const [searchValue, setSearchValue] = React.useState<string>("");
   const [dateFilter, setDateFilter] = React.useState<string[]>(["active"]);
+
   const isAdmin = useSelector((state: IState) => state.user.is_admin);
   const navigate = useNavigate();
 
@@ -103,36 +108,88 @@ const ChallengeList = () => {
             boxShadow: `10px 10px 0px black`,
             border: "solid black",
             width: "500px",
-            maxWidth: "90vw",
+            maxWidth: "90vw"
           }}
           square
         >
           <CardContent>
-            <TextField id="outlined-basic" label="Rechercher" variant="outlined"
-                       sx={{
-                         width: "100%",
-                       }}
-                       InputProps={{
-                         sx: {
-                           borderRadius: 0,
-                         }
-                       }}
-                       onChange={(event) => setSearchValue(event.target.value)}/>
-            <ToggleButtonGroup value={dateFilter} onChange={(event, newFilter) => setDateFilter(newFilter)}
-                               sx={{
-                                 width: "100%",
-                                 marginTop: 2,
-                               }}>
-              <ToggleButton value="active" sx={{borderRadius: 0, width: "100%"}}>
-                Actifs
-              </ToggleButton>
-              <ToggleButton value="upcoming" sx={{borderRadius: 0, width: "100%"}}>
-                À venir
-              </ToggleButton>
-              <ToggleButton value="finished" sx={{borderRadius: 0, width: "100%"}}>
-                Terminés
-              </ToggleButton>
-            </ToggleButtonGroup>
+            <Accordion sx={{
+              borderRadius: 0,
+              border: "none",
+              boxShadow: "none"
+            }}>
+              <AccordionSummary
+                expandIcon={<ArrowDownward sx={{margin: theme.spacing(2)}}/>}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                sx={{
+                  borderRadius: 0,
+                  backgroundColor: theme.palette.background.default,
+                  padding: 0,
+                  margin: 0,
+                }}>
+                <TextField id="outlined-basic" label="Rechercher" variant="outlined"
+                           sx={{
+                             width: "100%",
+                           }}
+                           InputProps={{
+                             sx: {
+                               borderRadius: 0,
+                             }
+                           }}
+                           onChange={(event) => setSearchValue(event.target.value)}/>
+              </AccordionSummary>
+              <AccordionDetails sx={{
+                borderRadius: 0,
+                backgroundColor: theme.palette.background.default,
+                borderBottom: "none",
+                padding: 0
+              }}>
+                <ToggleButtonGroup value={dateFilter} onChange={(event, newFilter) => setDateFilter(newFilter)}
+                                   sx={{
+                                     marginTop: theme.spacing(2),
+                                     width: "100%",
+                                     height: "60px"
+                                   }}>
+                  <ToggleButton value="active" sx={{borderRadius: 0, width: "100%"}}>
+                    Actifs
+                  </ToggleButton>
+                  <ToggleButton value="upcoming" sx={{borderRadius: 0, width: "100%"}}>
+                    À venir
+                  </ToggleButton>
+                  <ToggleButton value="finished" sx={{borderRadius: 0, width: "100%"}}>
+                    Terminés
+                  </ToggleButton>
+                </ToggleButtonGroup>
+                <Stack
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={2}
+                  sx={{
+                    margin: 0,
+                    marginTop: theme.spacing(2),
+                  }}
+                >
+                  <Select sx={{
+                    borderRadius: 0,
+                    width: "100px",
+                    height: "60px"
+                  }}>
+                    <MenuItem value={"asc"}><FontAwesomeIcon icon={faArrowUpShortWide} size="2x"/></MenuItem>
+                    <MenuItem value={"desc"}><FontAwesomeIcon icon={faArrowDownWideShort} size="2x"/></MenuItem>
+                  </Select>
+                  <Select sx={{
+                    borderRadius: 0,
+                    width: "100%",
+                    height: "60px"
+                  }}>
+                    <MenuItem value={"start"}>Début</MenuItem>
+                    <MenuItem value={"end"}>Fin</MenuItem>
+                  </Select>
+                </Stack>
+              </AccordionDetails>
+            </Accordion>
           </CardContent>
         </Card>
         {generateChallengeList(challengeList, searchValue, dateFilter)}
